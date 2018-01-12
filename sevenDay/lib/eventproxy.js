99f709c7ab53d6d4f56c8f8fb70fc9aa5128e63f
wangzhao <wangzhao@go-goal.com>
2018-01-12 18:13:30 +0800

    -

--- sevenDay/lib/eventproxy.js
+++ sevenDay/lib/eventproxy.js
@@ -6,6 +6,7 @@ var cnodeUrl = 'https://cnodejs.org';
 var ep = new eventproxy();
 var fs = require('fs');
 var path = require('path');
+var async = require('async');
 
 function GetJSON(params) {
     this.init();
@@ -22,7 +23,7 @@ GetJSON.prototype.init = function () {
         var topic_urls = [];
         var $ = cheerio.load(res.text);
         $('#topic_list .cell').each(function (i, n) {
-            // if (i > 10) return;
+            if (i > 10) return;
             var $n = $(n);
             var href = cnodeUrl + $n.find('.topic_title').attr('href');
             topic_urls.push(href);
@@ -33,13 +34,14 @@ GetJSON.prototype.init = function () {
                 var href = n[0],
                     _htmlTemplate = n[1];
                 var $ = cheerio.load(_htmlTemplate);
+                // console.log($('.topic_full_title').text())
                 return ({
-                    title: $('.topic_full_title').text(),
+                    title: $('.topic_full_title').text().trim().replace(/\\n{1,}/m, ''),
                     href: href,
-                    commit1: $('.reply_item').eq(0).find('.markdown-text').text() || '暂无评论'
+                    commit1: $('.reply_item').eq(0).find('.markdown-text').text().trim().replace(/\\n{1,}/m, '')
                 });
             });
-            console.log(info_data.length)
+            console.log(info_data);
             fs.writeFile(path.join(__dirname, '../doc', 'csdn.json'), '', () => {
             });
             fs.writeFile(path.join(__dirname, '../doc', 'csdn.json'), JSON.stringify(info_data), () => {
@@ -47,11 +49,17 @@ GetJSON.prototype.init = function () {
             });
         });
 
-        topic_urls.forEach(url => {
-            superagent.get(url).end((err, cres) => {
-                ep.emit('topic_event', [url, cres.text]);
+        topic_urls.forEach(function(topicUrl, i) {
+            superagent.get(topicUrl).end((err, cres) => {
+                ep.emit('topic_event', [topicUrl, cres.text]);
             });
+            // async.eachLimit(topic_urls, i, function (topicUrl, callback) {
+            //     superagent.get(topicUrl).end((err, cres) => {
+            //         ep.emit('topic_event', [topicUrl, cres.text]);
+            //     });
+            // })
         });
+        
 
 
     });
