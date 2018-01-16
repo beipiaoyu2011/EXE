5998ec6126dbf0dd61f527f657c5dce694feff5b
wangzhao <wangzhao@go-goal.com>
2018-01-16 10:18:40 +0800

    16

--- sevenDay/lib/eventproxy.js
+++ sevenDay/lib/eventproxy.js
@@ -23,7 +23,7 @@ GetJSON.prototype.init = function () {
         var topic_urls = [];
         var $ = cheerio.load(res.text);
         $('#topic_list .cell').each(function (i, n) {
-            if (i > 10) return;
+            // if (i > 10) return;
             var $n = $(n);
             var href = cnodeUrl + $n.find('.topic_title').attr('href');
             topic_urls.push(href);
@@ -49,17 +49,33 @@ GetJSON.prototype.init = function () {
             });
         });
 
-        topic_urls.forEach(function(topicUrl, i) {
+        topic_urls.forEach(function (topicUrl, i) {
             superagent.get(topicUrl).end((err, cres) => {
                 ep.emit('topic_event', [topicUrl, cres.text]);
             });
-            // async.eachLimit(topic_urls, i, function (topicUrl, callback) {
-            //     superagent.get(topicUrl).end((err, cres) => {
-            //         ep.emit('topic_event', [topicUrl, cres.text]);
-            //     });
-            // })
         });
-        
+
+        // 并发连接数的计数器
+        // var concurrencyCount = 0;
+        // var fetchUrl = function (url, callback) {
+        //     // delay 的值在 2000 以内，是个随机的整数
+        //     var delay = parseInt((Math.random() * 10000000) % 2000, 10);
+        //     concurrencyCount++;
+        //     console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', url, '，耗时' + delay + '毫秒');
+        //     setTimeout(function () {
+        //         concurrencyCount--;
+        //         callback(null, url);
+        //     }, delay);
+        // };
+
+        // async.mapLimit(topic_urls, 5, function (url, callback) {
+        //     fetchUrl(url, callback);
+        // }, function (err, result) {
+        //     console.log(err)
+        //     console.log('final')
+        //     console.log(result);
+        //     console.log(result.length);
+        // });
 
 
     });
