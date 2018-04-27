5aa3bc2c6a9ce6fea508b0e3c2ab51de6a9f8efe
wangzhao <wangzhao@go-goal.com>
2018-04-27 16:47:19 +0800

    9

---
+++ vue-server/public/app.js
@@ -0,0 +1,32 @@
+// 第 1 步：创建一个 Vue 实例
+const Vue = require('vue');
+const server = require('express')();
+const renderer = require('vue-server-renderer').createRenderer();
+const fs = require('fs');
+
+server.get('*', (req, res) => {
+    const app = new Vue({
+        data: {
+            url: req.url
+        },
+        template: fs.readdirSync('public/templates/index.template.html', 'utf-8')
+    });
+
+    // 第 2 步：创建一个 renderer
+    // 第 3 步：将 Vue 实例渲染为 HTML
+    renderer.renderToString(app, (err, html) => {
+        if (err) throw err
+        console.log(html)
+        res.send(`
+            <!DOCTYPE html>
+            <html lang="en">
+                <head><title>Hello</title></head>
+                <body>${html}</body>
+            </html>
+        `)
+    })
+});
+server.listen(9000);
+
+
+
